8572d8e49fc960bdb0c0c8729f7a9fdb67bcffae
Prayerson <prayernoveno@gmail.com>
2025-03-12 15:34:03 -0400

    Fixed paintRequiredCalculator to handle non-numeric input properly

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,7 +1,12 @@
 export default function paintRequiredCalculator(
-  area: number,
-  coveragePerLiter: number
+  area: any,
+  coveragePerLiter: any
 ): number {
+  if (isNaN(Number(area)) || isNaN(Number(coveragePerLiter))) {
+    throw new Error(
+      "Invalid input: Area and coverage per liter must be numbers"
+    );
+  }
   if (area < 0 || coveragePerLiter <= 0) {
     throw new Error(
       "Invalid input: Area and coverage per liter must be positive"
